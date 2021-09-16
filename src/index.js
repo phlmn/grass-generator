41e6e1d7f87d8895d4641f757994349755e69d9b
Philipp Mandler <info@philipp-mandler.com>
2021-09-17 01:29:48 +0200

    Add amount slider

--- src/index.js
+++ src/index.js
@@ -87,11 +87,11 @@ function ConfiguredGrass({ x, y }) {
     />
   );
 }
-function marks(min, max) {
+function marks(min, max, step) {
   const points = [];
 
-  for (let i = Math.ceil(min / 10); Math.floor(i * 10) <= max; i += 1) {
-    points[`${i * 10}`] = `${i * 10}`;
+  for (let i = Math.ceil(min / step); Math.floor(i * step) <= max; i += 1) {
+    points[`${i * step}`] = `${i * step}`;
   }
 
   return points;
@@ -113,8 +113,17 @@ function Configurator({ options, values, onChange }) {
       {options.map(option => {
         const value = values[option.name];
 
+        let type;
+        if (typeof value === 'number') {
+          type = 'slider';
+        } else if (value instanceof Array) {
+          type = 'range';
+        } else {
+          throw new Error(`Unknown type for ${option.name}`);
+        }
+
         return (
-          <div style={{ marginBottom: "2.5rem" }}>
+          <div key={option.name} style={{ marginBottom: "2.5rem" }}>
             <div
               style={{
                 fontSize: "12px",
@@ -126,19 +135,34 @@ function Configurator({ options, values, onChange }) {
             >
               <div>{option.name}</div>
               <div style={{ color: "#aaa" }}>
-                {value[0]} - {value[1]}
+                {type === 'range' && `${value[0]} - ${value[1]}`}
+                {type === 'slider' && value}
               </div>
             </div>
-            <Slider.Range
-              min={option.min || -20}
-              max={option.max || 20}
-              step={option.step || 1}
-              marks={marks(option.min || -20, option.max || 20)}
-              value={value}
-              onChange={value => {
-                onChange({ ...values, [option.name]: value });
-              }}
-            />
+            {type === 'range' && (
+              <Slider.Range
+                min={option.min || -20}
+                max={option.max || 20}
+                step={option.step || 1}
+                marks={marks(option.min || -20, option.max || 20, option.marksStep || 10)}
+                value={value}
+                onChange={value => {
+                  onChange({ ...values, [option.name]: value });
+                }}
+              />
+            )}
+            {type === 'slider' && (
+              <Slider
+                min={option.min || -20}
+                max={option.max || 20}
+                step={option.step || 1}
+                marks={marks(option.min || -20, option.max || 20, option.marksStep || 10)}
+                value={value}
+                onChange={value => {
+                  onChange({ ...values, [option.name]: value });
+                }}
+              />
+            )}
           </div>
         );
       })}
@@ -154,10 +178,12 @@ function App() {
   const [values, setValues] = useState({
     curve: [-10, 10],
     skew: [-20, 20],
-    segments: [2, 6]
+    segments: [2, 6],
+    amount: 300,
   });
 
-  const positions = new Array(200).fill(0).map(() => randomNumber(0, 1700));
+  const positions = new Array(values.amount).fill(0).map(() => randomNumber(0, 1700));
+
   return (
     <ConfigContext.Provider value={values}>
       <div className="App">
@@ -165,7 +191,8 @@ function App() {
           options={[
             { name: "curve", min: -30, max: 30 },
             { name: "skew", min: -40, max: 40 },
-            { name: "segments", min: 1, max: 20 }
+            { name: "segments", min: 1, max: 20 },
+            { name: "amount", min: 100, max: 800, marksStep: 100, step: 50 }
           ]}
           values={values}
           onChange={setValues}
